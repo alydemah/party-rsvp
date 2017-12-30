0f97450f368c9ed7c8edff89b12fd60e33c3b02a
Mohamed Aly <alydemah@gmail.com>
2017-12-30 19:41:15 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+import GuestList from './GuestList';
+//import './App.css';
+
+class App extends Component {
+
+  state = {
+    isFiltered: false,
+    pendingGuest: "",
+    guests: [
+      {
+        name: 'Aly',
+        isConfirmed: false,
+        isEditing: false
+      },
+      {
+        name: 'Jack',
+        isConfirmed: true,
+        isEditing: false
+
+      },
+      {
+        name: 'Tommy',
+        isConfirmed: false,
+        isEditing: true
+
+      }
+    ]
+  };
+
+  toggleGuestPropertyAt = (property, indexToChange) => 
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+            ...guest,
+            [property]: !guest[property]
+          };
+        }
+        return guest;
+      })
+    });
+
+    toggleConfirmationAt = index =>
+    this.toggleGuestPropertyAt("isConfirmed", index);
+    
+    removeGuestAt = index =>
+    this.setState({
+      guests: [
+        ...this.state.guests.slice(0, index),
+        ...this.state.guests.slice(index + 1)
+      ]
+    });
+
+    toggleEditingAt = index =>
+    this.toggleGuestPropertyAt("isEditing", index);
+  
+    setNameAt = (name, indexToChange) => 
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+            ...guest,
+            name
+          };
+        }
+        return guest;
+      })
+    });
+
+    toggleFilter = () =>
+    this.setState({ isFiltered: !this.state.isFiltered });
+
+    handleNameInput = e =>
+    this.setState({ pendingGuest: e.target.value });
+
+    newGuestSubmitHandler = e => {
+      e.preventDefault();
+      this.setState({
+        guests: [
+            {
+              name: this.state.pendingGuest,
+              isConfirmed: false,
+              isEditing: false
+            },
+            ...this.state.guests
+        ],
+        pendingGuest: ''
+      });
+    }
+
+  // Return the length of the guests Array
+  getTotalInvited = () => this.state.guests.length;
+  
+  // getAttendingGuests = () =>
+  // getUnconfirmedGuests = () =>
+
+
+  render() {
+    return (
+      <div className="App">
+      <header>
+        <h1>Party RSVP</h1>
+        <p>Add, Edit, Confirm Guests</p>
+        <form onSubmit={this.newGuestSubmitHandler}>
+            <input 
+            type="text" 
+            onChange={this.handleNameInput}
+            value={this.state.pendingGuest} 
+            placeholder="Invite Someone" />
+            <button type="submit" name="submit" value="submit">Submit</button>
+        </form>
+      </header>
+      <div className="main">
+        <div>
+          <h2>Invitees</h2>
+          <label>
+            <input 
+            type="checkbox"
+            onChange={this.toggleFilter}
+            checked={this.state.isFiltered} /> Hide those who haven't responded
+          </label>
+        </div>
+        <table className="counter">
+          <tbody>
+            <tr>
+              <td>Attending:</td>
+              <td>2</td>
+            </tr>
+            <tr>
+              <td>Unconfirmed:</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>Total:</td>
+              <td>3</td>
+            </tr>
+          </tbody>
+        </table>
+        <GuestList 
+        guests={this.state.guests} 
+        toggleConfirmationAt={this.toggleConfirmationAt}
+        toggleEditingAt={this.toggleEditingAt}
+        setNameAt={this.setNameAt}
+        isFiltered={this.state.isFiltered}
+        removeGuestAt={this.removeGuestAt}
+        pendingGuest={this.state.pendingGuest}
+         />
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
